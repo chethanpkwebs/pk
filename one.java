2e0af4012cc01572119a0b469fb45e0b3ea32273
chethan <chethan@gmail.com>
2012-01-01 00:49:42 +0530

    pk1

--- one.java
+++ one.java
@@ -1,5 +1,7 @@
 class hello
 public static void main(string[] args
 		{
-			system.out.println("hello welcom visual mood")
+			system.out.println("hello welcom visual mood");
+			system.out.println("hello welcom visual mood");
 		}
+	}
