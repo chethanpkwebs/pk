b30ddf50a06c856ec596a0f38336ed287d7700c1
chethanpkwebs <159775708+chethanpkwebs@users.noreply.github.com>
2024-02-12 16:37:41 +0530

    Update one.java

--- one.java
+++ one.java
@@ -3,5 +3,6 @@ public static void main(string[] args
 		{
 			system.out.println("hello welcom visual mood");
 			system.out.println("hello welcom visual mood");
-		}
+			system.out.println("hello welcom");
+}
 	}
